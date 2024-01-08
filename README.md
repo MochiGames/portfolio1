77d0000e198248f54722626a38999190768b76a2
magnetite1 <m-inquiries@hotmail.com>
2024-01-08 13:24:55 -0700

    hi

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# portfolio1
- 
+# portfolio
+ magnetite's portfolio
---
+++ script.js
@@ -0,0 +1,12 @@
+document.body.style.overflow = "auto"; // Enable scroll wheel after loading
+
+// Smoother scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
